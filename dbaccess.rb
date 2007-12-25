48e3aa5896548c359d9f9c7963a32e5c55a9a937
learn <learn@1433694b-0978-41e3-9398-0ffb1599e23b>
2007-12-25 18:51:25 +0000

    create
    git-svn-id: file:///Users/kunishi/tmp/svnroot/log_viewer/trunk@4 1433694b-0978-41e3-9398-0ffb1599e23b

---
+++ dbaccess.rb
@@ -0,0 +1,87 @@
+require 'postgres'
+
+class DBAccess
+  def initialize
+    host = 'localhost'
+    port = 5432
+    dbname = 'adel_v2'
+    user = 'learn'
+    pass = 'learn'
+    
+    begin
+      @conn = PGconn.connect(host,port,'','',dbname,user,pass)
+    rescue
+      exit
+    end
+  end
+  
+  def getByUserID(login)
+    res = @conn.exec("select id from users where \"login\" = 'konishi'")
+    return res.result[0][0]
+  end
+
+  def getByModuleName(module_id)
+    if module_id != -1
+      res = @conn.exec("select module_name from ent_modules where id = #{module_id}")
+      return res.result[0]
+    end
+  end
+
+  def getByModuleTimes(u_id , s_id)
+    res = @conn.exec("select ent_module_id , count(ent_module_id) from module_logs where user_id = '#{u_id}' and ent_seq_id='#{s_id}' group by ent_module_id order by ent_module_id")
+    return res.result
+  end
+
+  def getByModuleTimesAll(u_id)
+    res = @conn.exec("select ent_module_id , count(ent_module_id) from module_logs where user_id = '#{u_id}' group by ent_module_id order by ent_module_id")
+    return res.result
+  end
+
+  def getByTestMaxPoint(test_name)
+    res = @conn.exec("select max_sum_point from ent_tests where test_name = '#{test_name}'")
+    return res.result[0]
+  end
+
+  def getByTestID(test_name)
+    res = @conn.exec("select id from ent_tests where test_name = '#{test_name}'")
+    return res.result[0]
+  end
+
+  def getByTestPointAndTime(user_id , test_id)
+    res = @conn.exec("select created_on , sum_point from test_logs where user_id = '#{user_id}' and ent_test_id = '#{test_id}' order by created_on")
+    return res.result
+  end
+
+  def getByModuleTimeZoneAll(user_id)
+    res = @conn.exec("select module_logs.ent_seq_id , module_logs.ent_module_id , module_logs.created_on from module_logs,ent_modules where module_logs.user_id = '#{user_id}' and module_logs.ent_module_id = ent_modules.id order by created_on")
+    res_array = Array.new
+    i = 0
+    while(i < res.result.length - 1)
+      r = res.result[i]
+      if r[1].to_i != -1
+        res_array.push([ r[1],r[2], res.result[i+1][2] ])
+      end
+      i += 1
+    end
+    return res_array
+  end
+
+  def getByModuleTimeZone(user_id,seq_id)
+    res = @conn.exec("select module_logs.ent_seq_id , module_logs.ent_module_id , module_logs.created_on from module_logs,ent_modules where module_logs.user_id = '#{user_id}' and module_logs.ent_seq_id = '#{seq_id}' and module_logs.ent_module_id = ent_modules.id order by created_on")
+    res_array = Array.new
+    i = 0
+    while(i < res.result.length - 1)
+      r = res.result[i]
+      if r[1].to_i != -1
+        res_array.push([ r[1],r[2], res.result[i+1][2] ])
+      end
+      i += 1
+    end
+    return res_array
+  end
+
+  def dbClose
+    # DB Close
+    @conn.close
+  end
+end
---
+++ log.rb
@@ -0,0 +1,186 @@
+#!/usr/bin/ruby
+require 'cgi'
+require 'time'
+require 'dbaccess.rb'
+
+def xy_max(xy_array)
+  # xy_array 最大値を求める
+  max_value = 0
+  xy_array.each do |v|
+    if max_value < v[1].to_i
+      max_value = v[1].to_i
+    end
+  end
+  return max_value
+end
+
+def arrayKeys(array)
+  keys = Array.new
+  array.each do |v|
+    keys.push(v[0])
+  end
+  return keys
+end
+
+def arrayValues(array)
+  values = Array.new
+  array.each do |v|
+    values.push(v[1])
+  end
+  return values
+end
+
+def textEncode(array,max_value)
+  encode_array = Array.new
+  array.each do |v|
+    encode_array.push([v[0],v[1].to_i/max_value.to_f*100])
+  end
+  return encode_array
+end
+
+def setParams(chs,cht,chxt,array,max_value,y_split)
+  chParams = Hash.new
+  chParams.store("chs",chs)
+  chParams.store("cht",cht)
+  chParams.store("chxt",chxt)
+  
+  # make chd
+  chParams.store("chd","t:#{arrayValues(array).join(',')}")
+  # make chxl
+  if chxt == "x,y"
+    chxl = "0:|#{arrayKeys(array).join('|')}"
+  elsif chxt == "y,x"
+    chxl = "0:|#{arrayKeys(array).reverse.join('|')}"
+  end
+  chxl += "|1:|0"
+  n = 1
+  y_split.times do
+    chxl += "|#{(max_value.to_f/y_split*n).to_i}"
+    n += 1
+  end
+  chParams.store("chxl",chxl)
+  return chParams
+end
+
+# GoogleChart imgタグ生成関数
+# 引数：chs 画像サイズ,cht グラフタイプ,chxt x軸とy軸,array グラフの値のハッシュ
+# max_value プロット値の最大値,y軸の目盛の分割数
+def makeChart(chs,cht,chxt,array,max_value,y_split)
+  encode_array = textEncode(array,max_value)
+  chParams = setParams(chs,cht,chxt,encode_array,max_value,y_split)
+  
+  chartURL = "http://chart.apis.google.com/chart?"
+  chartURL += "chs=#{chParams["chs"]}"
+  chartURL += "&chd=#{chParams["chd"]}"
+  chartURL += "&cht=#{chParams["cht"]}"
+  chartURL += "&chxt=#{chParams["chxt"]}"
+  chartURL += "&chxl=#{chParams["chxl"]}"
+  
+  imgTag = "<img src='#{chartURL}' alt=\"Sample Chart\" />"
+  return imgTag
+end
+
+# Output function
+def view(v1,v2)
+  print "Content-type: text/html\n\n"
+  p v1
+  print "<br /><br />"
+  print v2
+end
+
+cgi = CGI.new
+
+# Get Message Input
+query_str = Hash.new
+query_str.store("login",cgi["login"])
+query_str.store("log_type",cgi["log_type"])
+query_str.store("view_type",cgi["view_type"])
+query_str.store("seq_id",cgi["seq_id"])
+query_str.store("test_name",cgi["test_name"])
+
+# make DB instance
+logDB = DBAccess.new
+
+# get user id
+user_id = logDB.getByUserID(query_str["login"])
+
+# Module or Test
+# Print to Module Log
+if query_str["log_type"] =~ /module/
+  if query_str["view_type"] =~ /number/
+    
+    if query_str["seq_id"] =~ /all/
+      mod_times = logDB.getByModuleTimesAll(user_id)
+    else
+      mod_times = logDB.getByModuleTimes(user_id , query_str["seq_id"])
+    end
+    
+    xy_array = Array.new
+    mod_times.each do |v|
+      mod_name = logDB.getByModuleName(v[0].to_i)
+      if mod_name
+        xy_array.push([mod_name,v[1]])
+      end
+    end
+    
+    max_value = xy_max(xy_array)
+
+    out_img_tag = makeChart("500x600","bhg","y,x",xy_array,max_value,max_value)    
+    
+  elsif query_str["view_type"] =~ /time/
+
+    if query_str["seq_id"] =~ /all/
+      mod_timezone = logDB.getByModuleTimeZoneAll(user_id)
+    else
+      mod_timezone = logDB.getByModuleTimeZone(user_id , query_str["seq_id"])
+    end
+    
+    mod_hash = Hash.new
+    mod_timezone.each do |v|
+      t1 = Time.parse(v[1])
+      t2 = Time.parse(v[2])
+
+      if mod_hash.key?(v[0])
+        mod_hash[v[0]] = mod_hash[v[0]] + (t2 - t1)
+      else
+        mod_hash.store(v[0],t2-t1)
+      end
+    end
+    xy_array = mod_hash.to_a.sort
+    y_split = 10
+    
+    xy_array.each do |v|
+      v[0] = logDB.getByModuleName(v[0].to_i)
+      v[1] = v[1].to_f/60
+      if 300 < v[1]
+        v[1] = 300
+      end
+    end
+
+    max_value = xy_max(xy_array)
+    out_img_tag = makeChart("500x600","bhg","y,x",xy_array,max_value,5)
+  end
+# Print to Test Log
+elsif query_str["log_type"] =~ /test/
+  # get test Max Point
+  max_point = logDB.getByTestMaxPoint(query_str["test_name"])[0]
+  # get test point , time from test_logs
+  point_and_time = logDB.getByTestPointAndTime(user_id , logDB.getByTestID(query_str["test_name"]))
+
+  xy_array = Array.new
+  i = 0
+  point_and_time.each do |pt|
+    x_time = Time.parse(pt[0])
+    xy_array.push([x_time.strftime("%m/%d-%H:%M"),pt[1]])
+    i+=1
+  end
+  
+  out_img_tag = makeChart("700x300","lc","x,y",xy_array,max_point,10)
+end
+
+# DB Connect Close
+logDB.dbClose
+  
+# View
+
+view(xy_array,out_img_tag)
