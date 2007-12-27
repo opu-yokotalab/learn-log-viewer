cae1e16ce3da273790797ed7d63890bb37c79737
learn <learn@1433694b-0978-41e3-9398-0ffb1599e23b>
2007-12-27 16:49:26 +0000

    y軸　小数点表示へ修正
    git-svn-id: file:///Users/kunishi/tmp/svnroot/log_viewer/trunk@37 1433694b-0978-41e3-9398-0ffb1599e23b

--- log.rb
+++ log.rb
@@ -55,7 +55,7 @@ def setParams(chs,cht,chxt,array,max_value,y_split)
   chxl += "|1:|0"
   n = 1
   y_split.times do
-    chxl += "|#{(max_value.to_f/y_split*n).to_i}"
+    chxl += "|#{max_value.to_f/y_split*n}"
     n += 1
   end
   chParams.store("chxl",chxl)
